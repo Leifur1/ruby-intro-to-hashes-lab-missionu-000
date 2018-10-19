3a801acc15f8e7b8f6177669d2b45e058fb82440
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-19 22:13:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,12 +17,10 @@ def monopoly_with_second_tier
 			:pieces => 4, :names => {},
 			:rent_in_dollars => {}
 			}
-
-
 	end
 
 	def monopoly_with_third_tier
-		
+
 		:railroads => {
 			:pieces => 4, :names => {
 				:reading_railroad => {},
